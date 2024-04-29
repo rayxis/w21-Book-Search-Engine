08c42426b57eb96a5d684368f74891f31405a00b
= <ray@rayxis.com>
2024-04-29 18:47:41 -0500

    Integrate Apollo Server to GraphQL API
    The codebase has been upgraded to use Apollo Server with GraphQL. This integration included addition of the respective schemas, resolvers, and authentication middleware, and setting of the API endpoint to '/graphql'. The server start message now also includes the GraphQL path on which it's listening.

--- server/server.js
+++ server/server.js
@@ -1,21 +1,42 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+// Import required modules
+const { ApolloServer }   = require('apollo-server-express');
+const express            = require('express');
+const path               = require('path');
+const db                 = require('./config/connection');
+const typeDefs           = require('./graphql/schema');     // GraphQL schemas
+const resolvers          = require('./graphql/resolvers');  // GraphQL resolvers, both Query and Mutation
+const { authMiddleware } = require('./utils/auth');         // Authentication middleware
 
-const app = express();
+// Setup express app
+const app  = express();
 const PORT = process.env.PORT || 3001;
 
+// URL encoded and JSON request body parsers
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
+// Serve static files only in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+	app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// Create a new instance of Apollo Server
+const server = new ApolloServer({
+	                                // The GraphQL schemas
+	                                typeDefs,
+	                                // The resolvers for executing the graphQL queries
+	                                resolvers,
+	                                // Make sure resolvers have access to the `authMiddleware`
+	                                context: authMiddleware
+                                });
 
+// Apply the Apollo GraphQL middleware and set the path to /graphql
+server.applyMiddleware({ app, path: '/graphql' });
+
+// Listen for connections on the specified port, once the database is open
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+	app.listen(PORT, () => {
+		// Log where the server is listening
+		console.log(`🌍 Now listening on localhost:${PORT}${server.graphqlPath}`);
+	});
 });
